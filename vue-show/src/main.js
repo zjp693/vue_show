80c2cf674135c12a51e2326a2393b84111c1c404
zjp693 <942680978@qq.com>
2020-11-12 23:02:49 +0800

    登陆页面

--- vue-show/src/main.js
+++ vue-show/src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css'
+import App from './App.vue'
 import router from "./router";
-
 createApp(App)
+  .use(Antd)
   .use(router)
-  .mount("#app");
+  .mount('#app')
---
+++ vue-show/src/views/Login.vue
@@ -0,0 +1,234 @@
+<template>
+  <a-layout>
+    <!-- 头部 -->
+    <a-layout-header></a-layout-header>
+    <!-- 主体 -->
+    <a-layout-content>
+      <!-- 登陆    -->
+      <a-row>
+        <a-col :span="6" :offset="10">
+          <!-- 表单 -->
+          <a-form :rules="rules" :model="form">
+            <!-- 标题 -->
+            <a-row class="login-title">
+              <a-col :span="24">
+                <h1><img src="../assets/logo.png" alt="" /> Ant Design</h1>
+                <span>Ant Design 是西湖区最具影响力的 Web 设计规范</span>
+
+                <!-- 登陆方式  -->
+                <a-row>
+                  <a-col :span="24">
+                    <a-tabs>
+                      <!--  手机号码登陆-->
+                      <a-tab-pane key="1" tab="手机号码登陆">
+                        <a-form-item :wrapperCol="{ span: 24 }" name="username">
+                          <a-input
+                            size="large"
+                            placeholder="请输入用户名"
+                            v-model:value="form.username"
+                          >
+                            <template #prefix
+                              ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
+                            /></template>
+                          </a-input>
+                        </a-form-item>
+                        <a-form-item :wrapperCol="{ span: 24 }" name="password">
+                          <a-input-password
+                            type="password"
+                            size="large"
+                            placeholder="请输入密码"
+                          >
+                            <template #prefix
+                              ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
+                            /></template>
+                          </a-input-password>
+                        </a-form-item>
+                      </a-tab-pane>
+                      <!-- 账号密码登陆 -->
+                      <a-tab-pane key="2" tab="账号密码登陆" force-render>
+                        <a-form-item :wrapperCol="{ span: 24 }">
+                          <a-input size="large" placeholder="请输入手机号">
+                            <template #prefix
+                              ><MobileOutlined
+                                style="color: rgba(0, 0, 0, 0.25)"
+                            /></template>
+                          </a-input>
+                        </a-form-item>
+
+                        <!-- 验证码 -->
+                        <a-row :gutter="16">
+                          <a-col class="gutter-row" :span="16">
+                            <a-form-item :wrapperCol="{ span: 24 }">
+                              <a-input size="large" placeholder="请输入验证码">
+                                <template #prefix
+                                  ><MailOutlined
+                                    style="color: rgba(0, 0, 0, 0.25)"
+                                /></template>
+                              </a-input>
+                            </a-form-item>
+                          </a-col>
+                          <a-col size="large" class="gutter-row" :span="8">
+                            <a-button size="large" style="padding: 0"
+                              >获取验证码</a-button
+                            >
+                          </a-col>
+                        </a-row>
+                      </a-tab-pane>
+                    </a-tabs>
+                  </a-col>
+                </a-row>
+              </a-col>
+            </a-row>
+            <!-- 忘记密码 -->
+            <a-row class="logo-forget">
+              <a-col :span="24">
+                <a-checkbox>自动登陆</a-checkbox>
+                <router-link to="#" style="float: right">忘记密码</router-link>
+              </a-col>
+            </a-row>
+            <!-- 确定按钮 -->
+            <a-row style="margin: 24px 0">
+              <a-col :span="24">
+                <a-form-item :wrapperCol="{ span: 24 }">
+                  <a-button block size="large" type="primary">确定 </a-button>
+                </a-form-item>
+              </a-col>
+            </a-row>
+            <!-- 其他方式 -->
+            <a-row style="margin: 24px 0">
+              <a-col :span="24">
+                <a-form-item :wrapperCol="{ span: 24 }">
+                  <span
+                    >其他登陆方式
+                    <AlipayCircleOutlined class="item-ico" />
+                    <TaobaoCircleOutlined class="item-ico" />
+                    <WeiboCircleOutlined class="item-ico"
+                  /></span>
+                  <router-link to="#" style="float: right"
+                    >注册账号</router-link
+                  >
+                </a-form-item>
+              </a-col>
+            </a-row>
+          </a-form>
+        </a-col>
+      </a-row>
+    </a-layout-content>
+    <a-layout-footer></a-layout-footer>
+  </a-layout>
+</template>
+
+<script>
+import {
+  UserOutlined,
+  LockOutlined,
+  MobileOutlined,
+  MailOutlined,
+  AlipayCircleOutlined,
+  TaobaoCircleOutlined,
+  WeiboCircleOutlined,
+} from "@ant-design/icons-vue";
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          {
+            //   required必须的
+            // trigger什么时候触发
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur",
+          },
+          {
+            min: 4,
+            max: 16,
+            message: "长度在4-16个字符之间",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            //   required必须的
+            // trigger什么时候触发
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+          {
+            min: 4,
+            max: 16,
+            message: "长度在4-16个字符之间",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  components: {
+    UserOutlined,
+    LockOutlined,
+    MobileOutlined,
+    MailOutlined,
+    AlipayCircleOutlined,
+    TaobaoCircleOutlined,
+    WeiboCircleOutlined,
+  },
+};
+</script>
+
+<style>
+.ant-layout {
+  height: 100%;
+  background-image: url(../assets/image/TVYTbAXWheQpRcWDaDMu.svg);
+}
+
+.ant-layout-header {
+  background-color: transparent;
+  margin-bottom: 32px;
+}
+.ant-form {
+  width: 365px;
+  height: 500px;
+}
+.login-title {
+  /* text-align: center; */
+}
+.ant-form h1 img {
+  width: 44px;
+  height: 44px;
+}
+.ant-form h1 {
+  position: relative;
+  top: 2px;
+  color: rgba(0, 0, 0, 0.85);
+  font-weight: 600;
+  font-size: 35px;
+  font-family: Avenir, helvetica neue, Arial, Helvetica, sans-serif;
+}
+.ant-form h1 span {
+  margin-top: 12px;
+  margin-bottom: 40px;
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 24px;
+}
+.item-ico {
+  /* font-size: 24px;
+  color: #848587;
+  position: relative;
+  top: 4px;
+  margin: 0 5px; */
+  font-size: 24px;
+  color: #848587;
+  position: relative;
+  top: 4px;
+  margin: 0 6px;
+}
+.item-ico:hover {
+  color: #40a9ff;
+}
+</style>
