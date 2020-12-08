902e674c85383cfea22467376f10d97f5d356b5e
zjp693 <942680978@qq.com>
2020-12-08 23:50:31 +0800

    图片预览

--- vue-show/src/components/GoodsAdd.vue
+++ vue-show/src/components/GoodsAdd.vue
@@ -78,16 +78,58 @@
               />
             </a-form-item>
           </a-tab-pane>
-          <a-tab-pane key="1" tab="商品参数"> Content of Tab 2 </a-tab-pane>
-          <a-tab-pane key="2" tab="商品属性"> Content of Tab 3 </a-tab-pane>
-          <a-tab-pane key="3" tab="商品图片"> Content of Tab 2 </a-tab-pane>
+          <a-tab-pane key="1" tab="商品参数">
+            <a-form-item
+              required
+              :label="item.attr_name"
+              :key="item.attr_id"
+              v-for="item in manyData"
+              name="goods_number"
+            >
+              <a-checkbox-group
+                v-model:value="item.attr_vals"
+                :options="item.attr_vals"
+              />
+            </a-form-item>
+          </a-tab-pane>
+          <a-tab-pane key="2" tab="商品属性">
+            <a-form-item
+              required
+              :label="item.attr_name"
+              :key="item.attr_id"
+              v-for="item in onlyData"
+              name="goods_number"
+            >
+              <a-input v-model:value="item.attr_vals" />
+            </a-form-item>
+          </a-tab-pane>
+          <a-tab-pane key="3" tab="商品图片">
+            <a-upload
+              accept="image/*"
+              action="http://www.wanlum.com:8888/api/private/v1/upload"
+              :headers="headerObj"
+              listType="picture"
+              @change="hanleUploadchang"
+              @preview="hanleUploadPrewview"
+            >
+              <a-button type="primary"> <upload-outlined /> 上传图片 </a-button>
+            </a-upload>
+          </a-tab-pane>
           <a-tab-pane key="4" tab="商品内容"> Content of Tab 3 </a-tab-pane>
         </a-tabs>
       </a-form>
     </a-card>
+    <!-- 预览图片弹出框 -->
+    <a-modal
+      title="图片预览"
+      :visible="previewVisible"
+      :footer="null"
+      @cancel="handleCancel"
+    >
+      <img alt="example" style="width: 100%" :src="prewviewPath" />
+    </a-modal>
   </a-layout>
 </template>
-
 <script>
 // 导入请求地址
 import { goods } from "@/api";
@@ -95,6 +137,7 @@ import { goods } from "@/api";
 // 导入请求方式
 import { httpGet } from "@/utils/http";
 import { message } from "ant-design-vue";
+import { UploadOutlined } from "@ant-design/icons-vue";
 export default {
   data() {
     return {
@@ -135,6 +178,11 @@ export default {
       options: [],
       activeKey: "0",
       cate_id: "",
+      manyData: [],
+      onlyData: [],
+      headerObj: { Authorization: window.sessionStorage.getItem("token") },
+      prewviewPath: "",
+      previewVisible: false,
     };
   },
   created() {
@@ -144,9 +192,8 @@ export default {
     handleReadCates() {
       httpGet(goods.GetCategories)
         .then((response) => {
-          console.log(response);
+          // console.log(response);
           let { meta, data } = response;
-
           if (meta.status == 200) {
             this.options = data;
           }
@@ -162,12 +209,34 @@ export default {
         this.activeKey = "0";
       } else {
         this.activeKey = activeKey;
-
+        // 商品参数
         if (activeKey == "1") {
-          console.log("商品参数");
+          // console.log("商品参数");
           httpGet(`categories/${this.cate_id}/attributes`, { sel: "many" })
             .then((response) => {
               console.log(response);
+              let { data, meta } = response;
+              if (meta.status == 200) {
+                data.forEach((ele) => {
+                  ele.attr_vals =
+                    ele.attr_vals.length == 0 ? [] : ele.attr_vals.split(" ");
+                });
+                this.manyData = data;
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+        // 商品属性
+        if (activeKey == "2") {
+          httpGet(`categories/${this.cate_id}/attributes`, { sel: "only" })
+            .then((response) => {
+              console.log(response);
+              let { data, meta } = response;
+              if (meta.status == 200) {
+                this.onlyData = data;
+              }
             })
             .catch((error) => {
               console.log(error);
@@ -181,6 +250,27 @@ export default {
         // console.log(this.cate_id);
       }
     },
+    hanleUploadchang(file) {
+      console.log(file);
+    },
+    hanleUploadPrewview(file) {
+      // console.log(file);
+      // let Urpath = ;
+
+      this.prewviewPath = file.response.data.url.replace(
+        /127.0.0.1/,
+        "www.wanlum.com"
+      );
+      this.previewVisible = true;
+      // console.log(Urpath);
+    },
+    handleCancel() {
+      this.previewVisible = false;
+      // this.prewviewPath = "";
+    },
+  },
+  components: {
+    UploadOutlined,
   },
 };
 </script>
