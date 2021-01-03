08b9aadd3ad07fb31d34ea6cf39586285949cb0b
zjp693 <942680978@qq.com>
2021-01-03 11:00:53 +0800

    路由更新

--- vue-show/src/router/index.js
+++ vue-show/src/router/index.js
@@ -1,8 +1,13 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory
+} from "vue-router";
 import Login from "../views/Login";
 
-const routes = [
-  { path: "/", redirect: "/login" },
+const routes = [{
+    path: "/",
+    redirect: "/login"
+  },
   // 登陆页
   {
     name: "Login",
@@ -13,8 +18,10 @@ const routes = [
   {
     path: "/home",
     component: () => import("../views/Home"),
-    children: [
-      { path: "/home", redirect: "/weclome" },
+    children: [{
+        path: "/home",
+        redirect: "/weclome"
+      },
       {
         name: "weclome",
         path: "/weclome",
@@ -84,7 +91,9 @@ router.beforeEach((to, from, next) => {
   //获取token
   let isAuthenticated = window.sessionStorage.getItem("token");
   // 去的不是login且没有授权就强制跳转到login页面
-  if (to.name !== "Login" && !isAuthenticated) next({ name: "Login" });
+  if (to.name !== "Login" && !isAuthenticated) next({
+    name: "Login"
+  });
   else next();
 });
 export default router;
